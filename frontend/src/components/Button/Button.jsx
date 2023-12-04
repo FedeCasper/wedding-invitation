f47e2e6eb50acd0308693dcbb1d5579d445ef182
FedeCasper <federicorouyere@gmail.com>
2023-12-03 21:19:57 -0300

    Button component

---
+++ frontend/src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+import './Button.css'
+
+const Button = () => {
+   return (
+      <button 
+         type="button" 
+         className="bg-[#5F7752] text-white text-lg regular-button unselectable">
+         Agendar
+      </button>
+   )
+}
+
+export default Button
--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,8 +1,9 @@
+import Button from "../components/Button/Button"
 
 const Layout = () => {
    return (
-      <div className=" h-min-screen w-full flex flex-col items-center overflow-hidden">
-         <section className=" h-36 text-sm font-bold bg-[#E1DFDB] relative z-20">
+      <div className=" h-min-screen flex flex-col items-center overflow-hidden">
+         <section className=" flex flex-col items-center h-36 w-full text-sm font-bold bg-[#E1DFDB]  z-20">
             <span>CIRCULO</span>
             <h1>Nos casamos</h1>
             <h2>Fede & Fede</h2>
@@ -10,40 +11,37 @@ const Layout = () => {
             <span>IMG</span>
          </section>
 
+         <section className="flex flex-col items-center w-full text-sm font-bold bg-[#C49F5F]   z-30">
+            <h5>Faltan</h5>
+            <div>CONTADOR</div>
+            <Button />
+            <div>IMG</div>
+            <h6>Ceremonia</h6>
+            <p>lorem</p>
+            <div>BUTTON</div>
+            <div>IMG</div>
+            <h6>Ceremonia</h6>
+            <p>lorem</p>
+            <div>BUTTON</div>
+         </section>
 
-         <section className="flex flex-col items-center h-36 text-sm font-bold bg-[#5F7752] relative z-20">
-            <section className="section2 h-fit w-full text-sm font-bold bg-[#C49F5F] rounded-b-full rounded-t-full absolute -top-10 z-30">
-               <h5>Faltan</h5>
-               <div>CONTADOR</div>
-               <div>BUTTON</div>
-               <div>IMG</div>
-               <h6>Ceremonia</h6>
-               <p>lorem</p>
-               <div>BUTTON</div>
-               <div>IMG</div>
-               <h6>Ceremonia</h6>
-               <p>lorem</p>
-               <div>BUTTON</div>
-            </section>
-
+         <section className="flex flex-col items-center h-36 w-full text-sm font-bold bg-[#5F7752]  z-20">
             <h6>Ceremonia</h6>
             <p>lorem</p>
             <div>IMG</div>
             <h6>Ceremonia</h6>
             <p>lorem</p>
             <div>BUTTON</div>
+         </section>
 
-            <section className=" h-fit w-full text-sm font-bold bg-[#E1DFDB] rounded-b-full rounded-t-full absolute -bottom-10 z-20">
+         <section className="flex flex-col items-center h-fit w-full text-sm font-bold bg-[#E1DFDB] z-20">
                <div>IMG</div>
                <h6>Ceremonia</h6>
                <p>lorem</p>
                <div>BUTTON</div>
                <h5>Lorem</h5>
                <h5>Lorem</h5>
-            </section>
          </section>
-
-
 
       </div>
    )
