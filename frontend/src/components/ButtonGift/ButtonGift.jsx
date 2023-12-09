f6ab0973a51da22c72702847f777ce7aa4d6ac15
FedeCasper <federicorouyere@gmail.com>
2023-12-09 16:57:28 -0300

    Modal animations, btn close, clipboard functionality

--- frontend/src/components/ButtonGift/ButtonGift.jsx
+++ frontend/src/components/ButtonGift/ButtonGift.jsx
@@ -1,30 +1,42 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ModalContext } from "../../context/ModalContext";
+import './ButtonGift.css'
 
 const ButtonGift = () => {
 
    const { modal, setModal } = useContext(ModalContext);
-
-   console.log(modal);
+   const [ ringMovement, setRingMovement ] = useState(false);
 
    const handleClick = () => {
       console.log('¡Se hizo clic en el botón!');
       setModal(true);
    };
 
+   const handleMovementInterval = () => {
+      setTimeout(() => {
+         setRingMovement(!ringMovement);
+         console.log('ringMovement', ringMovement);
+      }, 4000);
+   };
+
+   useEffect(() => {
+      handleMovementInterval();
+   }, [ringMovement]);
+
+
    return (
       <button 
-         onClick={handleClick}
-         className=" fixed h-12 w-12 z-50 top-[10px] right-[30px]
-         transition-all duration-300 ease-in-out hover:scale-110 active:scale-95">
+         onClick={ handleClick }
+         className={` fixed h-12 w-12 z-50 top-[10px] right-[30px]
+         transition-all duration-300 ease-in-out hover:scale-110 active:scale-95 ${ringMovement ? 'phone' : ''}`}>
             {
                modal ?
-                  <img src="/assets/images/gift-icon.png" alt="Gift icon"  /> :
+                  <img src="/assets/images/gift-icon.png" alt="Gift icon" /> :
                   <section className='relative'>
-                     <img src="/assets/images/heart-icon.png" alt="Heart icon" className='absolute top-0 left-0' />
+                     <img src="/assets/images/heart-icon.png" alt="Heart icon" className='absolute top-0 left-0 drop-shadow-md' />
                      <span className="relative flex h-3 w-3">
-                        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-amber-200 opacity-75 -top-1 -left-1"></span>
-                        <span className="relative inline-flex rounded-full h-3 w-3 bg-amber-400 -top-1 -left-1"></span>
+                        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75 -top-1 -left-1"></span>
+                        <span className="relative inline-flex rounded-full h-3 w-3 bg-red-700 -top-1 -left-1"></span>
                      </span>  
                   </section> 
             }
--- frontend/src/components/ImageComponent/ImageComponent.jsx
+++ frontend/src/components/ImageComponent/ImageComponent.jsx
@@ -5,22 +5,17 @@ const ImageComponent = ({ src, alt }) => {
    return (
       <AnimatedElement>
          <section className="relative">
-         <div className="loader -top-[5px] left-[50px]  animate-bounce">
+         <div className="loader -top-[5px] left-[50px]">
                <li className="ball"></li>
                <li className="ball"></li>
             </div>
-            {/* <div className="loader top-16 -right-6 rotate-45 z-0">
-               <li className="ball"></li>
-               <li className="ball"></li>
-               <li className="ball"></li>
-            </div> */}
-            <img src={src} alt={alt} className="relative mt-16 mb-6 h-32 z-40"/>
-            <div className="loader bottom-3 right-[10px] rotate-[270deg] z-10">
-               <li className="ball"></li>
-               <li className="ball"></li>
-               <li className="ball"></li>
-            </div>
+            <img src={src} alt={alt} className="relative mt-16 mb-6 h-32 "/>
             <div className=" loader bottom-12 -left-4 rotate-45 z-0">
+               <li className="ball"></li>
+               <li className="ball"></li>
+            </div>
+            <div className="loader bottom-3 right-[10px] rotate-[140deg] z-10">
+               <li className="ball"></li>
                <li className="ball"></li>
                <li className="ball"></li>
             </div>
