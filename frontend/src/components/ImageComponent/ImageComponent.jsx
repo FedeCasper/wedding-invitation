c4aebc526caf18cdbb08bcdc33237b857775c63e
FedeCasper <federicorouyere@gmail.com>
2023-12-17 19:01:06 -0300

    Texts in layout length

--- frontend/src/components/ImageComponent/ImageComponent.jsx
+++ frontend/src/components/ImageComponent/ImageComponent.jsx
@@ -4,7 +4,7 @@ import './ImageComponent.css'
 const ImageComponent = ({ src, alt, margin }) => {
    return (
       <AnimatedElement>
-         <section className={ `relative mb-8 ${margin === 'disabled' ? 'mt-1' : 'mt-8'} lg:mt-8` }>
+         <section className={ `relative mb-4 ${margin === 'disabled' ? 'mt-1' : 'mt-10'} lg:mt-8` }>
             <div className="loader top-[15px] left-[50px]">
                <li className="ball"></li>
                <li className="ball"></li>
