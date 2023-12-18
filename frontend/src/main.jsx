870eaccc0d88c5d5dee261144e413381b9b36bd5
FedeCasper <federicorouyere@gmail.com>
2023-12-18 17:10:21 -0300

    Working on back deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const baseUrl = '/';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 )
