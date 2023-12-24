0dbbc0b6746f2142278bd09a51bc30a259f010a2
FedeCasper <federicorouyere@gmail.com>
2023-12-24 14:12:07 -0300

    Fixing several bugs on manager

--- manager/src/pages/Layout.jsx
+++ manager/src/pages/Layout.jsx
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import ChurchIcon from '@mui/icons-material/Church';
+import CheckIcon from '@mui/icons-material/Check';
+
 
 const Layout = () => {
 
@@ -11,6 +14,7 @@ const Layout = () => {
       assistsToWedding: 0,
       notAssistsToWedding: 0,
       partner: [],
+      totalAssists: 0,
       drinks: {
          fernet: 0,
          gin_tonic: 0,
@@ -22,6 +26,9 @@ const Layout = () => {
       },
       food: [],
    });
+
+   console.log(executed);
+   console.log(Object.entries(stadisticData?.drinks));
 
    const handleData = () => {
       axios
@@ -36,9 +43,10 @@ const Layout = () => {
    };
 
    const handleStadistic = () => {
-      
+
       if (!executed) {
          fullData?.forEach( guest => {
+
             if (guest.assistChurch) {
                console.log("first")
                setStadisticData( prev => ({ ...prev, assistToChucrh: prev.assistToChucrh + 1 }));
@@ -46,34 +54,37 @@ const Layout = () => {
                setStadisticData( prev => ({ ...prev, notAssistToChucrh: prev.notAssistToChucrh + 1 }));
             }
 
-            // if (guest.assist) {
-            //    setStadisticData(prev => ({ ...prev, assistsToWedding: prev.assistsToWedding + 1 }));
-            // } else {
-            //    setStadisticData(prev => ({ ...prev, notAssistsToWedding: prev.notAssistsToWedding + 1 }));
-            // }
+            if (guest.assist) {
+               setStadisticData(prev => ({ ...prev, assistsToWedding: prev.assistsToWedding + 1 }));
+            } else {
+               setStadisticData(prev => ({ ...prev, notAssistsToWedding: prev.notAssistsToWedding + 1 }));
+            }
 
-            // if (guest.partner) {
-            //    setStadisticData(prev => ({ ...prev, partner: [...prev.partner, { fullName: guest.fullName, partnerName: guest.partnersName, partnerId: guest._id }] }));
-            // }
+            if (guest.partner) {
+               setStadisticData(prev => ({ ...prev, partner: [...prev.partner, { fullName: guest.fullName, partnerName: guest.partnersName, partnerId: guest._id }] }));
+            }
 
-            // if (guest.drinkPreferences) {
-            //    const updatedStadisticData = { ...stadisticData.drinks };
+            if (guest.drinkPreferences) {
+               const updatedStadisticData = { ...stadisticData.drinks };
+               console.log("updatedStadisticData", updatedStadisticData);
 
-            //    for(let drink in guest.drinkPreferences){
-            //       let formatKey = drink.slice(0, (drink.length -3))
+               for(let drink in guest.drinkPreferences){
+                  let formatKey = drink.slice(0, (drink.length -3))
 
-            //       if( guest.drinkPreferences[drink] ){
-            //          updatedStadisticData[formatKey] = (updatedStadisticData[formatKey] || 0) + 1;
-            //       }
+                  if( guest.drinkPreferences[drink] ){
+                     // updatedStadisticData[formatKey] = updatedStadisticData[formatKey] + 1;
+                     setStadisticData(prev => ({ ...prev, drinks: { ...prev.drinks, [formatKey]: prev.drinks[formatKey] + 1  }}));
+                  }
 
-            //    } 
-            //    setStadisticData(prev => ({ ...prev, drinks: updatedStadisticData }));
-            // }
+               } 
 
+
+            }
+            setExecuted(true);
          })
          
       }
-      setExecuted(true);
+
    }
 
    console.log(stadisticData);
@@ -247,11 +258,7 @@ const Layout = () => {
                      {/* ----- CARD 1 ----- */}
                      <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md">
                         <div className="bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-indigo-600 to-indigo-400 text-white shadow-indigo-500/40 shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center">
-                           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" className="w-6 h-6 text-white">
-                              <path d="M12 7.5a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z"></path>
-                              <path fillRule="evenodd" d="M1.5 4.875C1.5 3.839 2.34 3 3.375 3h17.25c1.035 0 1.875.84 1.875 1.875v9.75c0 1.036-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 011.5 14.625v-9.75zM8.25 9.75a3.75 3.75 0 117.5 0 3.75 3.75 0 01-7.5 0zM18.75 9a.75.75 0 00-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 00.75-.75V9.75a.75.75 0 00-.75-.75h-.008zM4.5 9.75A.75.75 0 015.25 9h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H5.25a.75.75 0 01-.75-.75V9.75z" clipRule="evenodd"></path>
-                              <path d="M2.25 18a.75.75 0 000 1.5c5.4 0 10.63.722 15.6 2.075 1.19.324 2.4-.558 2.4-1.82V18.75a.75.75 0 00-.75-.75H2.25z"></path>
-                           </svg>
+                           <ChurchIcon />
                         </div>
                         <div className="p-4 text-right">
                            <p className="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">Confirmados a la iglesia</p>
@@ -267,9 +274,7 @@ const Layout = () => {
                      {/* ----- CARD 2 ----- */}
                      <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md">
                         <div className="bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-indigo-600 to-indigo-400 text-white shadow-indigo-500/40 shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center">
-                           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" className="w-6 h-6 text-white">
-                              <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd"></path>
-                           </svg>
+                           <CheckIcon />
                         </div>
                         <div className="p-4 text-right">
                            <p className="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">Invitados confirmados</p>
@@ -325,7 +330,7 @@ const Layout = () => {
                      <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md overflow-hidden xl:col-span-2">
                         <div className="relative bg-clip-border rounded-xl overflow-hidden bg-transparent text-gray-700 shadow-none m-0 flex items-center justify-between p-6">
                            <div>
-                              <h6 className="block antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-blue-gray-900 mb-1">Mensajes de los invitados</h6>
+                              <h6 className="block antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-purple-900 mb-1">Mensajes de Amor ❤</h6>
                               <p className="antialiased font-sans text-sm leading-normal flex items-center gap-1 font-normal text-blue-gray-600">
                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" aria-hidden="true" className="h-4 w-4 text-blue-500">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
@@ -342,7 +347,7 @@ const Layout = () => {
                            </button>
                         </div>
                         <div className="p-6 overflow-x-scroll px-0 pt-0 pb-2">
-                           <table className="w-full min-w-[640px] table-auto">
+                           <table className="w-full table-auto">
                               <thead>
                                  <tr>
                                     <th className="border-b border-blue-gray-50 py-3 px-6 text-left">
@@ -384,7 +389,7 @@ const Layout = () => {
                      <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md overflow-hidden xl:col-span-2">
                         <div className="relative bg-clip-border rounded-xl overflow-hidden bg-transparent text-gray-700 shadow-none m-0 flex items-center justify-between p-6">
                            <div>
-                              <h6 className="block antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-blue-gray-900 mb-1">Invitados con acompañantes</h6>
+                              <h6 className="block antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-purple-900 mb-1">Invitados con acompañantes 👫</h6>
                               <p className="antialiased font-sans text-sm leading-normal flex items-center gap-1 font-normal text-blue-gray-600">
                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" aria-hidden="true" className="h-4 w-4 text-blue-500">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
@@ -401,7 +406,7 @@ const Layout = () => {
                            </button>
                         </div>
                         <div className="p-6 overflow-x-scroll px-0 pt-0 pb-2">
-                           <table className="w-full min-w-[640px] table-auto">
+                           <table className="w-full table-auto">
                               <thead>
                                  <tr>
                                     <th className="border-b border-gray-50 py-3 px-6 text-left">
@@ -426,6 +431,68 @@ const Layout = () => {
 
                                           <td className="py-3 px-5 border-b border-blue-gray-50">
                                              <p className="block antialiased font-sans text-xs font-medium text-blue-gray-600"> {guest?.partnerName}</p>
+                                          </td>
+                                       </tr>
+
+                                    ))
+                                 }
+
+                              </tbody>
+                           </table>
+                        </div>
+                     </div>
+                  </div>
+
+                  {/*  ----- DRINKS TABLE DATA ----- */}
+                  <div className="mb-4 grid grid-cols-1 gap-6 xl:grid-cols-3">
+                     <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md overflow-hidden xl:col-span-2">
+                        <div className="relative bg-clip-border rounded-xl overflow-hidden bg-transparent text-gray-700 shadow-none m-0 flex items-center justify-between p-6">
+                           <div>
+                              <h6 className="block antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-purple-900 mb-1">Bebidas elegidas 🍻</h6>
+                              <p className="antialiased font-sans text-sm leading-normal flex items-center gap-1 font-normal text-blue-gray-600">
+                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" aria-hidden="true" className="h-4 w-4 text-blue-500">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
+                                 </svg>
+                                 <strong>{(stadisticData?.partner).length} mensajes</strong> en total
+                              </p>
+                           </div>
+                           <button aria-expanded="false" aria-haspopup="menu" id=":r5:" className="relative middle none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-blue-gray-500 hover:bg-blue-500/10 active:bg-blue-500/30" type="button">
+                              <span className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
+                                 <svg xmlns="http://www.w3.org/2000/svg" fill="currenColor" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" aria-hidden="true" className="h-6 w-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"></path>
+                                 </svg>
+                              </span>
+                           </button>
+                        </div>
+                        <div className="p-6 overflow-x-scroll px-0 pt-0 pb-2">
+                           <table className="w-full table-auto">
+                              <thead>
+                                 <tr>
+                                    <th className="border-b border-gray-50 py-3 px-6 text-left">
+                                       <p className="block antialiased font-sans text-[11px] font-medium uppercase text-gray-400">Bebida</p>
+                                    </th>
+                                    <th className="border-b border-gray-50 py-3 px-6 text-left">
+                                       <p className="block antialiased font-sans text-[11px] text-center font-medium uppercase text-gray-400">N° veces elegida</p>
+                                    </th>
+                                 </tr>
+                              </thead>
+                              <tbody>
+
+                                 {
+                                    
+                                    Object.entries(stadisticData?.drinks)
+                                    .sort((a, b) => b[1] - a[1])
+                                    .map( drink => (
+
+                                       <tr key={drink[0]}>
+                                          <td className="py-3 px-5 border-b border-blue-gray-50">
+                                             <div className="flex items-center gap-4">
+                                                <p className="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-bold"> {drink[0]} </p>
+                                             </div>
+                                          </td>
+
+                                          <td className="py-3 px-5 border-b border-blue-gray-50">
+                                             <p className="block antialiased font-sans text-xs font-medium text-blue-gray-600"> {drink[1]}</p>
                                           </td>
                                        </tr>
 
