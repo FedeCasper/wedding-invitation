97ed772e931d51b125bcdf4eca2cf0c2a2ed6764
FedeCasper <federicorouyere@gmail.com>
2023-12-31 12:58:49 -0300

    Working on confimation form data in back

--- back/models/Guest.js
+++ back/models/Guest.js
@@ -4,14 +4,27 @@ let collection = 'invitados';
 
 let invitadoSchema = new Schema({
    fullName: {type: String, required: true},
-   assist: {type: Boolean, required: true},
-   drinkPreferences: {type: {} , required: true},
-   partner: {type: Boolean, required: true},
-   partnersName: {type: String, required: true},
-   assistChurch: {type: Boolean, required: true},
-   contact: {type: String, required: true},
-   foodPreferences: {type: {} , required: true},
-   message: {type: String, required: true},
+   assist: {type: Boolean, required: true, default: false},
+   drinkPreferences: {type: {} , required: true, default: {
+      ['fernet_🥤']: false,
+      ['gin_tonic_🍸']: false,
+      ['campari_🍹']: false,
+      ['vino_🍷']: false,
+      ['cerveza_🍺']: false,
+      ['no_tomo_alcohol_💧']: false,
+      ['otro_🥂']: '',
+   }},
+   partner: {type: Boolean, required: true, default: false},
+   partnersName: {type: String, required: true, default: ''},
+   assistChurch: {type: Boolean, required: true, default: false},
+   contact: {type: String, required: true, default: ''},
+   foodPreferences: {type: {} , required: true, default: {
+      ['como_sin_tac_❌🌾']: false,
+      ['soy_vegano_❌🥩']: false,
+      ['soy_vegetariano_💗🥑']: false,
+      ['otro_🍟🥩']: false,
+   }},
+   message: {type: String, required: true, default: ''},
 }, {
    timestamps: true
 });
