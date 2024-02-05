4e59709496ee5a3055d700fea9a0db29c988ce82
FedeCasper <federicorouyere@gmail.com>
2024-02-05 13:53:38 -0300

    Test

--- manager/vite.config.js
+++ manager/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      name: 'sweetalert2',
+      configureServer(server) {
+        server.middlewares.use(sweetalert2);
+      },
+    },
+  ],
+});
